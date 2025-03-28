0dfb47898ca03ab99188b1f3b96cd75f52b82c4d
Fredrik Rød <tilfredrik@icloud.com>
2025-03-28 02:52:23 +0100

    refactor(sysctl): using Effects not Either

--- src/PsInfo/Darwin/Sysctl.hs
+++ src/PsInfo/Darwin/Sysctl.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
-{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE ForeignFunctionInterface, DataKinds, FlexibleContexts, GADTs #-}
 module PsInfo.Darwin.Sysctl 
     ( getSysctl
     ) where
@@ -10,35 +9,44 @@ import Foreign
 import Foreign.C.Types
 import Foreign.C.Error (getErrno, errnoToIOError)
 import Foreign.C.String (peekCString)
-
+import Control.Monad.Freer (Members, Eff, send, runM)
+import Control.Monad.Freer.Error (Error, throwError, runError)
 
 foreign import ccall "sysctl"
     -- args: name, namelen, oldp, oldlenp, newp, newlenp
     c_sysctl :: Ptr CInt -> CUInt -> Ptr () -> Ptr CSize -> Ptr () -> CSize -> IO CInt
 
-getSysctlSize :: [CInt] -> IO (Either String CSize)
-getSysctlSize name = alloca $ \sizePtr -> do
-    let len = fromIntegral $ length name
-    namePtr <- listToPtr name
-    poke sizePtr (0 :: CSize)
-    kr <- c_sysctl namePtr len nullPtr sizePtr nullPtr 0
-    if kr == -1
-        then Left <$> getErrnoStr "getSysctlSize"
-        else Right <$> peek sizePtr
+getSysctlSize :: (Members '[Error String, IO] r) => [CInt] -> Eff r CSize
+getSysctlSize name = do 
+    es <- send $ alloca $ \sizePtr -> do
+        let len = fromIntegral $ length name
+        namePtr <- listToPtr name
+        poke sizePtr (0 :: CSize)
+        kr <- c_sysctl namePtr len nullPtr sizePtr nullPtr 0
+        if kr == -1
+            then Left <$> getErrnoStr "getSysctlSize"
+            else Right <$> peek sizePtr
+    case es of
+        (Left err) -> throwError err
+        (Right s) -> pure s
 
-getSysctl :: [CInt] -> IO (Either String String)
-getSysctl name = alloca $ \bufferPtr -> alloca $ \sizePtr -> do
-    let len = fromIntegral $ length name
-    namePtr <- listToPtr name
-    maybeSize <- getSysctlSize name
-    case maybeSize of
-        (Right size) -> do
-            poke sizePtr size
-            kr <- c_sysctl namePtr len (castPtr bufferPtr) sizePtr nullPtr 0
-            if kr == 0
-                then Right <$> peekCString bufferPtr
-                else Left <$> getErrnoStr "getSysctl"
-        (Left err) -> return $ Left err
+getSysctl :: (Members '[Error String, IO] r) => [CInt] -> Eff r String
+getSysctl name = do
+    ec <- send $ alloca $ \bufferPtr -> alloca $ \sizePtr -> do
+        let len = fromIntegral $ length name
+        namePtr <- listToPtr name
+        maybeSize <- runM $ runError $ getSysctlSize name
+        case maybeSize of
+            (Left err) -> return $ Left err
+            (Right size) -> do
+                poke sizePtr size
+                kr <- c_sysctl namePtr len (castPtr bufferPtr) sizePtr nullPtr 0
+                if kr == 0
+                    then Right <$> peekCString bufferPtr
+                    else Left <$> getErrnoStr "getSysctl"
+    case ec of 
+        (Left err) -> throwError err
+        (Right c) -> pure c
 
 listToPtr :: (Storable a) => [a] -> IO (Ptr a)
 listToPtr xs = do
--- test/PsInfo/Darwin/Sysctl/SysctlTests.hs
+++ test/PsInfo/Darwin/Sysctl/SysctlTests.hs
@@ -2,18 +2,24 @@ module PsInfo.Darwin.Sysctl.SysctlTests (tests) where
 
 import PsInfo.Darwin.Sysctl
 
-import Data.Either (isRight, isLeft)
 import qualified Test.HUnit as H
+
+import Control.Monad.Freer (runM)
+import Control.Monad.Freer.Error (runError)
 
 testSysctlSuccess :: H.Test
 testSysctlSuccess = H.TestCase $ do 
-    r <- getSysctl [1, 1]
-    H.assertBool "sysctl 1,1 should give a value" $ isRight r
+    es <- runM $ runError $ getSysctl [1, 1]
+    case es of
+        (Left err) -> H.assertFailure $ "sysctl 1,1 should give a value: " ++ err
+        (Right _) -> pure ()
 
 testSysctlFail :: H.Test
 testSysctlFail = H.TestCase $ do
-    r <- getSysctl [1, 1, 1]
-    H.assertBool "sysctl 1,1 should give a value" $ isLeft r
+    es <- (runM $ runError $ getSysctl [1, 1, 1] :: IO (Either String String))
+    case es of
+        (Left _) -> pure ()
+        (Right s) -> H.assertFailure $ "sysctl 1,1,1 should not give a value: " ++ show s
 
 tests :: H.Test
 tests = H.TestList 
