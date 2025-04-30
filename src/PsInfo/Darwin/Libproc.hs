44289f4148a06f85d3d1e15bf1639af6e0df7227
Fredrik Rød <tilfredrik@icloud.com>
2025-04-30 12:34:16 +0200

    refactor(libproc): now using util + refactoring

--- src/PsInfo/Darwin/Libproc.hs
+++ src/PsInfo/Darwin/Libproc.hs
@@ -1,58 +1,39 @@
-{-# LANGUAGE ForeignFunctionInterface, DataKinds, FlexibleContexts, GADTs #-}
+{-# LANGUAGE ForeignFunctionInterface, DataKinds #-}
 module PsInfo.Darwin.Libproc
-    ( getListPids
-    , getProcTaskInfo
-    , ProcTaskInfo
-    , pti_virtual_size
-    , pit_resident_size
-    , pti_total_user
-    , pti_total_system
-    , pti_threads_user
-    , pti_threads_system
-    , pti_policy
-    , pti_faults
-    , pti_pageins
-    , pti_cow_faults
-    , pti_messages_sent
-    , pti_messages_received
-    , pti_syscalls_mach
-    , pti_syscalls_unix
-    , pti_csw
-    , pti_threadnum
-    , pti_numrunning
-    , pti_priority
+    ( getProcTaskInfo
+    , ProcTaskInfo (..)
+    , getListPids
     ) where
--- https://github.com/phracker/MacOSX-SDKs/blob/041600eda65c6a668f66cb7d56b7d1da3e8bcc93/MacOSX10.5.sdk/usr/include/libproc.h#L85
 
-import Control.Monad.Freer ( send, Eff, Members )
-import Control.Monad.Freer.Error ( throwError, Error )
-import Foreign
-import Foreign.C.Types
-import Foreign.C.Error (getErrno, errnoToIOError)
+import Foreign (Ptr, Storable(..), alloca, allocaArray, castPtr, peekArray, nullPtr)
+import Foreign.C.Types (CInt(..), CUInt(..), CULong)
+import Foreign.C.Error (errnoToIOError, getErrno)
+import Foreign.C (peekCString)
 
-foreign import ccall "proc_pidinfo"
-    -- args: pid, flavor, arg, buffer, buffersize
-    c_proc_pidinfo :: CInt -> CInt -> CInt -> Ptr () -> CInt -> IO CInt
+import Control.Monad.Freer (Eff, Members, send)
+import Control.Monad.Freer.Error (Error)
+
+import PsInfo.Util.Effect (eitherToEff)
 
 data ProcTaskInfo = ProcTaskInfo
-    { pti_virtual_size :: CULong
-    , pit_resident_size :: CULong
-    , pti_total_user :: CULong
-    , pti_total_system :: CULong
-    , pti_threads_user :: CULong
-    , pti_threads_system :: CULong
-    , pti_policy :: CInt
-    , pti_faults :: CInt
-    , pti_pageins :: CInt
-    , pti_cow_faults :: CInt
-    , pti_messages_sent :: CInt
+    { pti_virtual_size      :: CULong
+    , pti_resident_size     :: CULong
+    , pti_total_user        :: CULong
+    , pti_total_system      :: CULong
+    , pti_threads_user      :: CULong
+    , pti_threads_system    :: CULong
+    , pti_policy            :: CInt
+    , pti_faults            :: CInt
+    , pti_pageins           :: CInt
+    , pti_cow_faults        :: CInt
+    , pti_messages_sent     :: CInt
     , pti_messages_received :: CInt
-    , pti_syscalls_mach :: CInt
-    , pti_syscalls_unix :: CInt
-    , pti_csw :: CInt
-    , pti_threadnum :: CInt
-    , pti_numrunning :: CInt
-    , pti_priority :: CInt
+    , pti_syscalls_mach     :: CInt
+    , pti_syscalls_unix     :: CInt
+    , pti_csw               :: CInt
+    , pti_threadnum         :: CInt
+    , pti_numrunning        :: CInt
+    , pti_priority          :: CInt
     } deriving (Eq, Show)
 
 instance Storable ProcTaskInfo where
@@ -80,27 +61,20 @@ instance Storable ProcTaskInfo where
         <*> peekByteOff ptr (sizeOf (undefined :: CULong) * 6 + sizeOf (undefined :: CInt) * 11)
     poke _ _ = error "poke not implemented"
 
-foreign import ccall "proc_listpids"
-    -- args: type, type_info, buffer, buffer_size
+foreign import ccall unsafe "libproc.h proc_pidinfo"
+    -- int proc_pidinfo(int pid, int flavor, uint64_t arg, void *buffer, int buffersize)
+    c_proc_pidinfo :: CInt -> CInt -> CUInt -> Ptr () -> CInt -> IO CInt
+
+foreign import ccall unsafe "libproc.h proc_listpids"
+    -- int proc_listpids(uint32_t type, uint32_t typeinfo, void *buffer, int buffersize)
     c_proc_listpids :: CInt -> CInt -> Ptr CInt -> CInt -> IO CInt
+
+foreign import ccall unsafe "libproc.h proc_pidpath"
+    -- int proc_pidpath(int pid, void *buffer, uint32_t buffersize)
+    c_proc_pidpath :: CInt -> Ptr () -> CUInt -> IO CInt
 
 _PROC_PIDTASKINFO :: CInt
 _PROC_PIDTASKINFO = 4
-
-getProcTaskInfo :: (Members '[Error String, IO] r) => CInt -> Eff r ProcTaskInfo
-getProcTaskInfo pid = do
-    epti <- send $ alloca $ \ptr -> do
-        let ptrSize = fromIntegral (sizeOf (undefined :: ProcTaskInfo))
-        br <- c_proc_pidinfo pid _PROC_PIDTASKINFO 0 (castPtr ptr) ptrSize
-        if br == -1
-            then do
-                errno <- getErrno
-                let err = errnoToIOError "getProcTaskInfo" errno Nothing Nothing
-                return $ Left $ show err
-            else Right <$> peek ptr
-    case epti of
-        (Left err) -> throwError err
-        (Right pti) -> pure pti
 
 _PROC_ALL_PIDS :: CInt
 _PROC_ALL_PIDS = 1
@@ -108,23 +82,28 @@ _PROC_ALL_PIDS = 1
 _PROC_MAX_PID_COUNT :: Int
 _PROC_MAX_PID_COUNT = 4096
 
+_PROC_PIDPATHINFO_MAXSIZE :: CUInt
+_PROC_PIDPATHINFO_MAXSIZE = 1024 * 4
+
+getProcTaskInfo :: (Members '[Error String, IO] r) => CInt -> Eff r ProcTaskInfo
+getProcTaskInfo pid = eitherToEff $ alloca $ \ptr -> do
+    let ptrSize = fromIntegral (sizeOf (undefined :: ProcTaskInfo))
+    br <- c_proc_pidinfo pid _PROC_PIDTASKINFO 0 (castPtr ptr) ptrSize
+    if br == -1
+        then Left <$> getErrnoString "getProcTaskInfo"
+        else Right <$> peek ptr
+
 getListPids :: Members '[Error String, IO] r => Eff r [CInt]
-getListPids = do
-    epids <- send $ allocaArray _PROC_MAX_PID_COUNT $ \bufferPtr -> do
-        let bufferSize = fromIntegral $ _PROC_MAX_PID_COUNT * sizeOf (undefined :: CInt)
+getListPids = do 
+    bufferSize <- send $ c_proc_listpids _PROC_ALL_PIDS 0 nullPtr 0
+    eitherToEff $ allocaArray (fromIntegral bufferSize `div` sizeOf (undefined :: CInt)) $ \bufferPtr -> do
         bytes <- c_proc_listpids _PROC_ALL_PIDS 0 bufferPtr bufferSize
         if bytes == -1
-            then do
-                errno <- getErrno
-                let err = errnoToIOError "getListPids" errno Nothing Nothing
-                return $ Left $ show err
+            then Left <$> getErrnoString "getListPids"
             else do
                 let numPids = fromIntegral bytes `div` sizeOf (undefined :: CInt)
-                pids <- ptrToList bufferPtr numPids
+                pids <- peekArray numPids bufferPtr
                 return $ Right pids
-    case epids of
-        (Left err) -> throwError err
-        (Right pids) -> pure pids
 
-ptrToList :: Ptr CInt -> Int -> IO [CInt]
-ptrToList bufferPtr len = mapM (peekElemOff bufferPtr) [0 .. (len - 1)]
+getErrnoString :: String -> IO String
+getErrnoString who = getErrno >>= \err -> pure (show $ errnoToIOError who err Nothing Nothing)
