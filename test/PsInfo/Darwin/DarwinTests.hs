c0f709d05565bcea58286616f493a1ded33b8300
Fredrik Rød <tilfredrik@icloud.com>
2025-04-30 13:09:10 +0200

    test(darwin): added + refactoring tests

--- test/PsInfo/Darwin/DarwinTests.hs
+++ test/PsInfo/Darwin/DarwinTests.hs
@@ -1,16 +1,93 @@
 module PsInfo.Darwin.DarwinTests (tests) where
 
+import Control.Monad.Freer (runM)
+import Control.Monad.Freer.Error (runError)
+import Test.HUnit
+
+import PsInfo.Darwin
+import PsInfo.Util.Test
+import PsInfo.Util.Types (PID (..))
 import qualified PsInfo.Darwin.Libproc.LibprocTests as L (tests)
 import qualified PsInfo.Darwin.Mach.MachTests as M (tests)
 import qualified PsInfo.Darwin.Rusage.RusageTests as R (tests)
 import qualified PsInfo.Darwin.Sysctl.SysctlTests as S (tests)
 
-import qualified Test.HUnit as H
+testGetCPUActiveTime :: Test
+testGetCPUActiveTime = TestCase $ do
+    eactive <- runM $ runError getCPUActiveTime
+    assertRight "getCPUActiveTime" eactive
+    mapM_ (assertPositive "getCPUActiveTime") eactive
 
-tests :: H.Test
-tests = H.TestList 
+testGetCPUTotalTime :: Test
+testGetCPUTotalTime = TestCase $ do
+    etotal <- runM $ runError getCPUTotalTime
+    assertRight "getCPUTotalTime" etotal
+    mapM_ (assertPositive "getCPUTotalTime") etotal
+
+testGetCPUUsage :: Test
+testGetCPUUsage = TestCase $ do
+    eusage <- runM $ runError $ getCPUUsage 1000000
+    assertRight "getCPUUsage 1s" eusage
+    mapM_ (assertBetween "getCPUUsage 1s" 0 1) eusage
+    mapM_ (assertPositive "getCPUUsage 1s") eusage
+
+testGetMemUsage :: Test
+testGetMemUsage = TestCase $ do
+    eusage <- runM $ runError getMemUsage
+    assertRight "getMemUsage" eusage
+    mapM_ (assertBetween "getMemUsage" 0 1) eusage
+    mapM_ (assertPositive "getMemUsage") eusage
+
+testGetPIDs :: Test
+testGetPIDs = TestCase $ do
+    epids <- runM $ runError getPIDs
+    assertRight "getPIDs" epids
+    mapM_ (assertNotEmpty "getPIDs") epids
+
+testGetProcessCPUUsage :: Test
+testGetProcessCPUUsage = TestCase $ do
+    eusage <- runM $ runError $ getProcessCPUUsage (PID 1) 10000
+    assertRight "getProcessCPUUsage 1 10ms" eusage
+    mapM_ (assertBetween "getProcessCPUUsage 1 10ms" 0 1) eusage
+
+testGetProcessMemUsage :: Test
+testGetProcessMemUsage = TestCase $ do
+    eusage <- runM $ runError $ getProcessMemUsage (PID 1)
+    assertRight "getProcessMemUsage 1" eusage
+    mapM_ (assertBetween "getProcessMemUsage 1" 0 1) eusage
+
+testGetProcessName :: Test
+testGetProcessName = TestCase $ do
+    ename <- runM $ runError $ getProcessName (PID 1)
+    assertRight "getProcessName 1" ename
+    mapM_ (assertNotEmpty "getProcessName 1") ename
+
+testGetProcessTime :: Test
+testGetProcessTime = TestCase $ do
+    etime <- runM $ runError $ getProcessTime (PID 1)
+    assertRight "getProcessTime 1" etime
+    mapM_ (assertPositive "getProcessTime 1") etime
+
+testGetWallTime :: Test
+testGetWallTime = TestCase $ do
+    ewall <- runM $ runError getWallTime
+    assertRight "getWallTime" ewall
+    mapM_ (assertPositive "getWallTime") ewall
+
+tests :: Test
+tests = TestList 
     [ L.tests
     , M.tests
     , R.tests
     , S.tests
+    , testGetCPUActiveTime
+    , testGetCPUTotalTime
+    , testGetCPUUsage
+    , testGetMemUsage
+    , testGetPIDs
+    , testGetProcessCPUUsage
+    , testGetProcessMemUsage
+    , testGetProcessName
+    , testGetProcessTime
+    , testGetWallTime
     ]
