db899bc7ce99befe37984f5fd0123febfc1b678d
Fredrik Rød <tilfredrik@icloud.com>
2025-04-30 18:22:09 +0200

    chore(mach): removed unused code

--- src/PsInfo/Darwin/Mach.hs
+++ src/PsInfo/Darwin/Mach.hs
@@ -1,11 +1,6 @@
 {-# LANGUAGE ForeignFunctionInterface, DataKinds #-}
 module PsInfo.Darwin.Mach
-    ( getSelfPID
-    , getTaskForPid
-    , getTaskInfoForTask
-    , TimeValue (..)
-    , TaskBasicInfo (..)
-    , getHostStatistics
+    ( getHostStatistics
     , HostStatistics (..)
     , getMachTimebaseInfo
     , MachTimebaseInfo (..)
@@ -13,52 +8,12 @@ module PsInfo.Darwin.Mach
     ) where
 
 import Foreign (Ptr, Storable(..), alloca, castPtr)
-import Foreign.C.Types (CInt(..), CLong, CUInt(..), CULLong(..))
+import Foreign.C.Types (CInt(..), CUInt(..), CULLong(..))
 
 import Control.Monad.Freer (Eff, Members)
 import Control.Monad.Freer.Error (Error)
 
 import PsInfo.Util.Effect (eitherToEff)
-
-data TimeValue = TimeValue
-    { tv_seconds :: CInt
-    , tv_microseconds :: CInt
-    } deriving (Show, Eq)
-
-instance Storable TimeValue where
-    sizeOf _ = sizeOf (undefined :: CInt) * 2
-    alignment _ = alignment (undefined :: CInt)
-    peek ptr = TimeValue
-        <$> peekByteOff ptr 0
-        <*> peekByteOff ptr (sizeOf (undefined :: CInt))
-    poke ptr (TimeValue s ms) = do
-        pokeByteOff ptr 0 s
-        pokeByteOff ptr (sizeOf (undefined :: CInt)) ms
-
-data TaskBasicInfo = TaskBasicInfo
-    { tbi_virtural_memory :: CLong
-    , tbi_resident_size :: CLong
-    , tbi_resident_size_max :: CLong
-    , tbi_user_time :: TimeValue
-    , tbi_system_time :: TimeValue
-    , tbi_policy :: CInt
-    , tbi_suspend_count :: CInt
-    } deriving (Show, Eq)
-
-instance Storable TaskBasicInfo where
-    sizeOf _ = sizeOf (undefined :: CLong) * 3
-             + sizeOf (undefined :: TimeValue) * 2
-             + sizeOf (undefined :: CInt) * 2
-    alignment _ = alignment (undefined :: CLong)
-    peek ptr = TaskBasicInfo
-        <$> peekByteOff ptr 0
-        <*> peekByteOff ptr (sizeOf (undefined :: CLong))
-        <*> peekByteOff ptr (sizeOf (undefined :: CLong) * 2)
-        <*> peekByteOff ptr (sizeOf (undefined :: CLong) * 3)
-        <*> peekByteOff ptr (sizeOf (undefined :: CLong) * 3 + sizeOf (undefined :: TimeValue))
-        <*> peekByteOff ptr (sizeOf (undefined :: CLong) * 3 + sizeOf (undefined :: TimeValue) * 2)
-        <*> peekByteOff ptr (sizeOf (undefined :: CLong) * 3 + sizeOf (undefined :: TimeValue) * 2 + sizeOf (undefined :: CInt))
-    poke _ _ = error "poke not implemented"
 
 data HostStatistics = HostStatistics
     { hsUser   :: CUInt
@@ -94,18 +49,6 @@ foreign import ccall unsafe "sys/mach.h mach_host_self"
     -- mach_port_t mach_host_self(void)
     c_mach_host_self :: IO CUInt
 
-foreign import ccall unsafe "sys/mach.h pid_for_task"
-    -- kern_return_t pid_for_task(mach_port_name_t t, int *x)
-    c_pid_for_task :: CUInt -> Ptr CInt -> IO CInt
-
-foreign import ccall unsafe "sys/mach.h task_for_pid"
-    -- kern_return_t task_for_pid(mach_port_name_t target_tport, int pid, mach_port_name_t *t)
-    c_task_for_pid :: CUInt -> CInt -> Ptr CUInt -> IO CInt
-
-foreign import ccall unsafe "sys/mach.h task_info"
-    -- kern_return_t task_info(task_name_t target_task, task_flavor_t flavor, task_info_t task_info_out, mach_msg_type_number_t *task_info_outCnt)
-    c_task_info :: CUInt -> CUInt -> Ptr () -> Ptr CUInt -> IO CInt
-
 foreign import ccall unsafe "sys/mach.h host_statistics"
     -- kern_return_t host_statistics(host_t host_priv, host_flavor_t flavor, host_info_t host_info_out, mach_msg_number_t host_info_outCnt)
     c_host_statistics :: CUInt -> CInt -> Ptr () -> Ptr CUInt -> IO CInt
@@ -118,38 +61,11 @@ foreign import ccall unsafe "mach/mach_time.h mach_absolute_time"
     -- uint64_t mach_absolute_time(void)
     c_mach_absolute_time :: IO CULLong
 
-_MACH_TASK_BASIC_INFO :: CUInt
-_MACH_TASK_BASIC_INFO = 20
-
 _HOST_CPU_LOAD_INFO :: CInt
 _HOST_CPU_LOAD_INFO = 3
 
 _CPU_STATE_MAX :: CUInt
 _CPU_STATE_MAX = 4
-
-getSelfPID :: Members '[Error String, IO] r => Eff r CInt
-getSelfPID = eitherToEff $ alloca $ \pidPtr -> do 
-    host <- c_mach_host_self
-    kr <- c_pid_for_task host (castPtr pidPtr)
-    if kr == 0
-        then Right <$> peek pidPtr
-        else return $ Left $ "getSelfPID failed! reason: " ++ showKR kr
-
-getTaskForPid :: Members '[Error String, IO] r => CInt -> Eff r CUInt
-getTaskForPid pid = eitherToEff $ alloca $ \ptr -> do
-    host <- c_mach_host_self
-    kr <- c_task_for_pid host pid ptr
-    if kr == 0
-        then Right <$> peek ptr
-        else return $ Left $ "getTaskForPid failed! reason:" <> showKR kr
-
-getTaskInfoForTask :: Members '[Error String, IO] r => CUInt -> Eff r TaskBasicInfo
-getTaskInfoForTask task = eitherToEff $ alloca $ \infoPtr -> alloca $ \sizePtr -> do
-    poke sizePtr (fromIntegral (sizeOf (undefined :: TaskBasicInfo)) :: CUInt)
-    kr <- c_task_info task _MACH_TASK_BASIC_INFO (castPtr infoPtr) sizePtr
-    if kr == 0
-        then Right <$> peek infoPtr
-        else return $ Left $ "getTaskInfoForTask failed! reason:" <> showKR kr
 
 getHostStatistics :: Members '[Error String, IO] r => Eff r HostStatistics
 getHostStatistics = eitherToEff $ alloca $ \statsPtr -> alloca $ \sizePtr -> do
--- test/PsInfo/Darwin/Mach/MachTests.hs
+++ test/PsInfo/Darwin/Mach/MachTests.hs
@@ -1,8 +1,5 @@
 module PsInfo.Darwin.Mach.MachTests (tests) where
 
-import Control.Monad (when)
-import Data.Either (isRight)
-
 import Control.Monad.Freer (runM)
 import Control.Monad.Freer.Error (runError)
 import Test.HUnit
@@ -10,30 +7,11 @@ import Test.HUnit
 import PsInfo.Darwin.Mach
 import PsInfo.Util.Test
 
-testGetTaskForPidInitSuccess :: Test
-testGetTaskForPidInitSuccess = TestCase $ do 
-    et <- runM $ runError $ getTaskForPid 1
-    assertRight "getTaskForPid 1" et
-
-testGetTaskInfoForTaskInitSuccess :: Test
-testGetTaskInfoForTaskInitSuccess = TestCase $ do 
-    et <- runM $ runError $ getTaskForPid 1
-    assertRight "getTaskForPid 1" et
-    when (isRight et) $ do
-        (Right t) <- pure et
-        eti <- runM $ runError $ getTaskInfoForTask t
-        assertRight ("getTaskInfoForTask " ++ show t) eti
-
 testGetHostStatistics :: Test
 testGetHostStatistics = TestCase $ do
     estat <- runM $ runError getHostStatistics
     assertRight "getHostStatistics" estat
     mapM_ (assertPositive "getHostStatistics (system)" . hsSystem) estat
-
-testGetHostSelf :: Test
-testGetHostSelf = TestCase $ do
-    epid <- runM $ runError getSelfPID
-    assertRight "getSelfPID" epid
 
 testGetMachTimebaseInfo :: Test
 testGetMachTimebaseInfo = TestCase $ do
@@ -49,10 +27,7 @@ testGetMachAbsoluteTime = TestCase $ do
 
 tests :: Test
 tests = TestList
-    [ testGetTaskForPidInitSuccess
-    , testGetTaskInfoForTaskInitSuccess
-    , testGetHostStatistics
-    , testGetHostSelf
+    [ testGetHostStatistics
     , testGetMachTimebaseInfo
     , testGetMachAbsoluteTime
     ]
