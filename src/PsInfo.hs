cb8dc43e473a3ab7a3c4e1d2bc62b06a7de39ef0
Fredrik Rød <tilfredrik@icloud.com>
2025-04-30 13:02:21 +0200

    feat(psinfo): exporting system-based mathods of darwin and linux

--- src/PsInfo.hs
+++ src/PsInfo.hs
@@ -1,38 +1,84 @@
 {-# LANGUAGE OverloadedStrings, DataKinds, FlexibleContexts, GADTs #-}
-module PsInfo where
+module PsInfo 
+    ( getCPUActiveTime
+    , getCPUTotalTime
+    , getCPUUsage
+    , getMemUsage
+    , getPIDs
+    , getProcessCPUUsage
+    , getProcessMemUsage
+    , getProcessName
+    , getProcessTime
+    , getWallTime
+    ) where
 
--- import qualified PsInfo.Darwin as D
-import qualified PsInfo.Linux as L
-
-import Control.Monad.Freer ( Eff, Members )
-import Control.Monad.Freer.Error ( throwError, Error )
+import Control.Monad.Freer (Eff, Member, Members)
+import Control.Monad.Freer.Error (Error, throwError)
 import System.Info (os)
 
-type Percent = Double
-type PID = Int
-type MicroSec = Int
+import PsInfo.Util.Types (MicroSecond, Percent, PID)
+import qualified PsInfo.Darwin as D
+import qualified PsInfo.Linux as L
 
-getPIDs :: (Members '[Error String, IO] r) => Eff r [PID]
-getPIDs = case os of
-    "linux" -> L.getPIDs
-    _ -> throwError $ "Not implemented for " ++ os
+getCPUActiveTime :: Members '[Error String, IO] r => Eff r MicroSecond
+getCPUActiveTime = case os of
+    "linux"  -> L.getCPUActiveTime
+    "darwin" -> D.getCPUActiveTime
+    _        -> throwNotImplemented
 
-getProcessCPUUsage :: (Members '[Error String, IO] r) => PID -> MicroSec -> Eff r Percent
-getProcessCPUUsage pid s = case os of
-    "linux" -> L.getProcessCPUUsage pid s
-    _ -> throwError $ "Not implemented for " ++ os
+getCPUTotalTime :: Members '[Error String, IO] r => Eff r MicroSecond
+getCPUTotalTime = case os of
+    "linux"  -> L.getCPUTotalTime
+    "darwin" -> D.getCPUTotalTime
+    _        -> throwNotImplemented
 
-getProcessMemUsage :: (Members '[Error String, IO] r) => PID -> Eff r Percent
-getProcessMemUsage pid = case os of
-    "linux" -> L.getProcessMemUsage pid
-    _ -> throwError $ "Not implemented for " ++ os
+getCPUUsage :: Members '[Error String, IO] r => MicroSecond -> Eff r Percent
+getCPUUsage = case os of
+    "linux"  -> L.getCPUUsage
+    "darwin" -> D.getCPUUsage
+    _        -> const throwNotImplemented
 
-getMemUsage :: (Members '[Error String, IO] r) => Eff r Percent
+getMemUsage :: Members '[Error String, IO] r => Eff r Percent
 getMemUsage = case os of
-    "linux" -> L.getMemUsage
-    _ -> throwError $ "Not implemented for " ++ os
+    "linux"  -> L.getMemUsage
+    "darwin" -> D.getMemUsage
+    _        -> throwNotImplemented
 
-getCPUUsage :: (Members '[Error String, IO] r) => MicroSec -> Eff r Percent
-getCPUUsage delay = case os of
-    "linux" -> L.getCPUUsage delay
-    _ -> throwError $ "Not implemented for " ++ os
+getPIDs :: Members '[Error String, IO] r => Eff r [PID]
+getPIDs = case os of
+    "linux"  -> L.getPIDs
+    "darwin" -> D.getPIDs
+    _        -> throwNotImplemented
+
+getProcessCPUUsage :: Members '[Error String, IO] r => PID -> MicroSecond -> Eff r Percent
+getProcessCPUUsage = case os of
+    "linux"  -> L.getProcessCPUUsage
+    "darwin" -> D.getProcessCPUUsage
+    _        -> const $ const throwNotImplemented
+
+getProcessMemUsage :: Members '[Error String, IO] r => PID -> Eff r Percent
+getProcessMemUsage = case os of
+    "linux"  -> L.getProcessMemUsage
+    "darwin" -> D.getProcessMemUsage
+    _        -> const throwNotImplemented
+
+getProcessName :: Members '[Error String, IO] r => PID -> Eff r String
+getProcessName = case os of
+    "linux"  -> L.getProcessName
+    "darwin" -> D.getProcessName
+    _        -> const throwNotImplemented
+
+getProcessTime :: Members '[Error String, IO] r => PID -> Eff r MicroSecond
+getProcessTime = case os of
+    "linux"  -> L.getProcessTime
+    "darwin" -> D.getProcessTime
+    _        -> const throwNotImplemented
+
+getWallTime :: Members '[Error String, IO] r => Eff r MicroSecond
+getWallTime = case os of
+    "linux"  -> L.getWallTime
+    "darwin" -> D.getWallTime
+    _        -> throwNotImplemented
+
+throwNotImplemented :: Member (Error String) r => Eff r a
+throwNotImplemented = throwError $ "Not implemented for the os: " ++ os
