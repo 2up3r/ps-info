d1945931f4deb8691870c2f0a57623d27b3b2df1
Fredrik Rød <tilfredrik@icloud.com>
2025-04-30 13:14:22 +0200

    refactor(psinfo): refactoring tests

--- test/PsInfo/PsInfoTests.hs
+++ test/PsInfo/PsInfoTests.hs
@@ -1,13 +1,13 @@
-module PsInfo.PsInfoTests ( tests ) where
+module PsInfo.PsInfoTests (tests) where
+
+import System.Info (os)
+import Test.HUnit
 
 import qualified PsInfo.Darwin.DarwinTests as D
 import qualified PsInfo.Linux.LinuxTests as L
 
-import qualified Test.HUnit as H
-import System.Info ( os )
-
-tests :: H.Test
+tests :: Test
 tests = case os of
     "darwin" -> D.tests
     "linux" -> L.tests
-    _ -> H.TestList []
+    _ -> TestList []
