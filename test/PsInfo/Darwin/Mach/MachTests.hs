08e4874d5f698ef9c4ec7dd53f0a7657cb680657
Fredrik Rød <tilfredrik@icloud.com>
2025-04-30 13:05:31 +0200

    test(mach): added new + refactor

--- test/PsInfo/Darwin/Mach/MachTests.hs
+++ test/PsInfo/Darwin/Mach/MachTests.hs
@@ -1,30 +1,58 @@
 module PsInfo.Darwin.Mach.MachTests (tests) where
 
-import PsInfo.Darwin.Mach
-import qualified Test.HUnit as H
+import Control.Monad (when)
+import Data.Either (isRight)
+
 import Control.Monad.Freer (runM)
 import Control.Monad.Freer.Error (runError)
+import Test.HUnit
 
-testGetTaskForPidInitSuccess :: H.Test
-testGetTaskForPidInitSuccess = H.TestCase $ do 
+import PsInfo.Darwin.Mach
+import PsInfo.Util.Test
+
+testGetTaskForPidInitSuccess :: Test
+testGetTaskForPidInitSuccess = TestCase $ do 
     et <- runM $ runError $ getTaskForPid 1
-    case et of
-        (Left err) -> H.assertFailure $ "getTaskForPid 1 (initial process) should give a value: " ++ err
-        (Right _) -> pure ()
+    assertRight "getTaskForPid 1" et
 
-testGetTaskInfoForTaskInitSuccess :: H.Test
-testGetTaskInfoForTaskInitSuccess = H.TestCase $ do 
+testGetTaskInfoForTaskInitSuccess :: Test
+testGetTaskInfoForTaskInitSuccess = TestCase $ do 
     et <- runM $ runError $ getTaskForPid 1
-    case et of 
-        (Left err) -> H.assertFailure $ "getTaskForPid 1 (init process) should give a value: " ++ err
-        (Right t) -> do
-            eti <- runM $ runError $ getTaskInfoForTask t
-            case eti of
-                (Left err) -> H.assertFailure $ "getTaskInfoForTask pid=1 (initial process) should give a value: " ++ err
-                (Right _) -> pure ()
+    assertRight "getTaskForPid 1" et
+    when (isRight et) $ do
+        (Right t) <- pure et
+        eti <- runM $ runError $ getTaskInfoForTask t
+        assertRight ("getTaskInfoForTask " ++ show t) eti
 
-tests :: H.Test
-tests = H.TestList
+testGetHostStatistics :: Test
+testGetHostStatistics = TestCase $ do
+    estat <- runM $ runError getHostStatistics
+    assertRight "getHostStatistics" estat
+    mapM_ (assertPositive "getHostStatistics (system)" . hsSystem) estat
+
+testGetHostSelf :: Test
+testGetHostSelf = TestCase $ do
+    epid <- runM $ runError getSelfPID
+    assertRight "getSelfPID" epid
+
+testGetMachTimebaseInfo :: Test
+testGetMachTimebaseInfo = TestCase $ do
+    emti <- runM $ runError getMachTimebaseInfo
+    assertRight "getMachTimebaseInfo" emti
+    mapM_ (assertPositive "getMachTimebaseInfo (denom)" . mti_denom) emti
+    mapM_ (assertPositive "getMachTimebaseInfo (number)" . mti_number) emti
+
+testGetMachAbsoluteTime :: Test
+testGetMachAbsoluteTime = TestCase $ do
+    mat <- getMachAbsoluteTime
+    assertPositive "getMachAbsoluteTime" mat
+
+tests :: Test
+tests = TestList
     [ testGetTaskForPidInitSuccess
     , testGetTaskInfoForTaskInitSuccess
+    , testGetHostStatistics
+    , testGetHostSelf
+    , testGetMachTimebaseInfo
+    , testGetMachAbsoluteTime
     ]
