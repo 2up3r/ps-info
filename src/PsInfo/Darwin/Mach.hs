3e257703ec9612184cddf6acddea75798aea9d19
Fredrik Rød <tilfredrik@icloud.com>
2025-04-30 12:56:10 +0200

    feat(mach): added extra funtions (self-pid, host-stats, timebase-info, absolute time)

--- src/PsInfo/Darwin/Mach.hs
+++ src/PsInfo/Darwin/Mach.hs
@@ -1,8 +1,15 @@
 {-# LANGUAGE ForeignFunctionInterface, DataKinds #-}
 module PsInfo.Darwin.Mach
+    ( getSelfPID
+    , getTaskForPid
     , getTaskInfoForTask
     , TimeValue (..)
     , TaskBasicInfo (..)
+    , getHostStatistics
+    , HostStatistics (..)
+    , getMachTimebaseInfo
+    , MachTimebaseInfo (..)
+    , getMachAbsoluteTime
     ) where
 
 import Foreign (Ptr, Storable(..), alloca, castPtr)
@@ -53,9 +60,43 @@ instance Storable TaskBasicInfo where
         <*> peekByteOff ptr (sizeOf (undefined :: CLong) * 3 + sizeOf (undefined :: TimeValue) * 2 + sizeOf (undefined :: CInt))
     poke _ _ = error "poke not implemented"
 
+data HostStatistics = HostStatistics
+    { hsUser   :: CUInt
+    , hsSystem :: CUInt
+    , hsIdle   :: CUInt
+    , hsNice   :: CUInt
+    } deriving (Eq, Show)
+
+instance Storable HostStatistics where
+    sizeOf _ = sizeOf (undefined :: CInt) * 4
+    alignment _ = alignment (undefined :: CInt)
+    peek ptr = HostStatistics
+        <$> peekByteOff ptr 0
+        <*> peekByteOff ptr (sizeOf (undefined :: CInt))
+        <*> peekByteOff ptr (sizeOf (undefined :: CInt) * 2)
+        <*> peekByteOff ptr (sizeOf (undefined :: CInt) * 3)
+    poke _ _ = error "poke not implemented"
+
+data MachTimebaseInfo = MachTimebaseInfo
+    { mti_denom :: CUInt
+    , mti_number :: CUInt
+    } deriving (Eq, Show)
+
+instance Storable MachTimebaseInfo where
+    sizeOf _ = sizeOf (undefined :: CUInt) * 2
+    alignment _ = alignment (undefined :: CUInt)
+    peek ptr = MachTimebaseInfo
+        <$> peekByteOff ptr 0
+        <*> peekByteOff ptr (sizeOf (undefined :: CUInt))
+    poke _ _ = error "poke not implemented"
+
 foreign import ccall unsafe "sys/mach.h mach_host_self"
     -- mach_port_t mach_host_self(void)
     c_mach_host_self :: IO CUInt
+
+foreign import ccall unsafe "sys/mach.h pid_for_task"
+    -- kern_return_t pid_for_task(mach_port_name_t t, int *x)
+    c_pid_for_task :: CUInt -> Ptr CInt -> IO CInt
 
 foreign import ccall unsafe "sys/mach.h task_for_pid"
     -- kern_return_t task_for_pid(mach_port_name_t target_tport, int pid, mach_port_name_t *t)
@@ -65,6 +106,18 @@ foreign import ccall unsafe "sys/mach.h task_info"
     -- kern_return_t task_info(task_name_t target_task, task_flavor_t flavor, task_info_t task_info_out, mach_msg_type_number_t *task_info_outCnt)
     c_task_info :: CUInt -> CUInt -> Ptr () -> Ptr CUInt -> IO CInt
 
+foreign import ccall unsafe "sys/mach.h host_statistics"
+    -- kern_return_t host_statistics(host_t host_priv, host_flavor_t flavor, host_info_t host_info_out, mach_msg_number_t host_info_outCnt)
+    c_host_statistics :: CUInt -> CInt -> Ptr () -> Ptr CUInt -> IO CInt
+
+foreign import ccall unsafe "mach/mach_time.h mach_timebase_info"
+    -- kern_return_t mach_timebase_info(mach_timebase_info_t info)
+    c_mach_timebase_info :: Ptr MachTimebaseInfo -> IO CInt
+
+foreign import ccall unsafe "mach/mach_time.h mach_absolute_time"
+    -- uint64_t mach_absolute_time(void)
+    c_mach_absolute_time :: IO CULLong
+
 _MACH_TASK_BASIC_INFO :: CUInt
 _MACH_TASK_BASIC_INFO = 20
 
@@ -73,6 +126,14 @@ _HOST_CPU_LOAD_INFO = 3
 
 _CPU_STATE_MAX :: CUInt
 _CPU_STATE_MAX = 4
+
+getSelfPID :: Members '[Error String, IO] r => Eff r CInt
+getSelfPID = eitherToEff $ alloca $ \pidPtr -> do 
+    host <- c_mach_host_self
+    kr <- c_pid_for_task host (castPtr pidPtr)
+    if kr == 0
+        then Right <$> peek pidPtr
+        else return $ Left $ "getSelfPID failed! reason: " ++ showKR kr
 
 getTaskForPid :: Members '[Error String, IO] r => CInt -> Eff r CUInt
 getTaskForPid pid = eitherToEff $ alloca $ \ptr -> do
@@ -89,6 +150,25 @@ getTaskInfoForTask task = eitherToEff $ alloca $ \infoPtr -> alloca $ \sizePtr -
     if kr == 0
         then Right <$> peek infoPtr
         else return $ Left $ "getTaskInfoForTask failed! reason:" <> showKR kr
+
+getHostStatistics :: Members '[Error String, IO] r => Eff r HostStatistics
+getHostStatistics = eitherToEff $ alloca $ \statsPtr -> alloca $ \sizePtr -> do
+    host <- c_mach_host_self
+    poke sizePtr _CPU_STATE_MAX
+    kr <- c_host_statistics host _HOST_CPU_LOAD_INFO (castPtr statsPtr) sizePtr
+    if kr == 0
+        then Right <$> peek statsPtr
+        else return $ Left $ "getHostStatistics failed! reason: " <> showKR kr
+
+getMachTimebaseInfo :: Members '[Error String, IO] r => Eff r MachTimebaseInfo
+getMachTimebaseInfo = eitherToEff $ alloca $ \ptr -> do
+    kr <- c_mach_timebase_info ptr
+    if kr == 0
+        then Right <$> peek ptr
+        else return $ Left $ "getMachTimebaseInfo failed! reason: " <> showKR kr
+
+getMachAbsoluteTime :: IO Integer
+getMachAbsoluteTime = fromIntegral <$> c_mach_absolute_time
 
 showKR :: CInt -> String
 showKR 0 = "KERN_SUCCESS"
